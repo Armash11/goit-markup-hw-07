b8967ebf3b41b1d040a71aee178e833fe726b7bb
Armash11 <124906592+Armash11@users.noreply.github.com>
2023-04-23 19:38:25 +0200

    hw-07

--- js/menu.js
+++ js/menu.js
@@ -4,10 +4,8 @@
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
   };
-
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
-
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
     document.body.classList.toggle("no-scroll");
